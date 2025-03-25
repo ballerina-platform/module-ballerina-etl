74e402c5dd4fd8ea374834f65fbc860dac5f84d5
InduwaraGayashan001 <induwaragayashan@gmail.com>
2025-03-25 14:40:13 +0530

    Add new constants

--- native/src/main/java/io/ballerina/stdlib/etl/utils/Constants.java
+++ native/src/main/java/io/ballerina/stdlib/etl/utils/Constants.java
@@ -1,8 +1,14 @@
 package io.ballerina.stdlib.etl.utils;
 
 public class Constants {
-    
+
     public static final String IDLE_TIMEOUT_ERROR = "IdleTimeoutError";
     public static final String CLIENT_CONNECTION_ERROR = "ClientConnectionError";
- 
+    public static final String STRING = "string";
+    public static final String REGEX_MULTIPLE_WHITESPACE = "\\s+";
+    public static final String SINGLE_WHITESPACE = " ";
+    public static final String GROUP_APPROXIMATE_DUPLICATES = "groupApproximateDuplicatesFunc";
+    public static final String STANDARDIZE_DATA = "standardizeDataFunc";
+    public static final String CATEGORIZE_SEMANTIC = "categorizeSemanticFunc";
+    public static final String EXTRACT_FROM_UNSTRUCTURED_DATA = "extractFromUnstructuredDataFunc";
 }
